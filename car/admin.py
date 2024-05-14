8e3157b9e97f4b728286bb6afd51ff3fb06f2002
mussieKelati <mussiekelati24@gmail.com>
2024-05-14 03:44:53 +0300

    last commit

--- car/admin.py
+++ car/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from car.models import Car
+# Register your models here
+class CarAdmin(admin.ModelAdmin):
+    fieldsets = [
 
-# Register your models here.
+        ('TIME INFORMATION', {'fields': ['year']}),
+        ('CAR INFORMATION', {'fields': ['brand']})
+    ]
+     
+
+admin.site.register(Car,CarAdmin)
+
+
+
